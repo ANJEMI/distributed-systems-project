59079d7d74947ff9fa6a87e9804a86d01dd4999e
Jose <concepcijoseantonio@gmail.com>
2025-01-02 22:29:43 -0500

    add: check of existing networks

--- start.sh
+++ start.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Definición de colores
+# Colors
 RESET="\033[0m"
 GREEN="\033[92m"
 YELLOW="\033[93m"
 RED="\033[91m"
 BLUE="\033[94m"
 
-# Emojis
 ROCKET="🚀"
 CHECK_MARK="✅"
-WARNING="⚠️"
+WARNING="🚧"
 
-# Función para verificar el estado de salida
+# Check status function
 check_status() {
     if [ $? -ne 0 ]; then
         echo -e "${RED}${WARNING} Error occurred during the last operation.${RESET}"
@@ -20,14 +19,27 @@ check_status() {
     fi
 }
 
-# Crear redes
-echo -e "${BLUE}${ROCKET} Creating networks...${RESET}"
-docker network create bitclients --subnet 10.0.10.0/24
-check_status
-docker network create bitservers --subnet 10.0.11.0/24
-check_status
+create_network() {
+    local network_name=$1
+    local subnet=$2
 
-# Configurar el router
+    if docker network ls | grep -q "$network_name"; then
+        echo -e "${WARNING} ${YELLOW}Network $network_name already exists. Removing it...${RESET}"
+        docker network rm "$network_name"
+        check_status
+    fi
+
+    docker network create "$network_name" --subnet "$subnet"
+    check_status
+}
+
+# Create the networks
+echo -e "${BLUE}${ROCKET} Creating networks...${RESET}"
+create_network "bitclients" "10.0.10.0/24"
+create_network "bitservers" "10.0.11.0/24"
+
+
+# Set up the router
 echo -e "${BLUE}${ROCKET} Setting up router...${RESET}"
 docker build -t router -f src/router/router.Dockerfile .
 check_status
@@ -38,23 +50,23 @@ check_status
 docker network connect --ip 10.0.11.254 bitservers router
 check_status
 
-# Configurar cliente y servidor
+# Set up client and server
 echo -e "${BLUE}${ROCKET} Building client and server images...${RESET}"
 docker build -t bitserver -f server.dockerfile .
 check_status
 docker build -t bitclient -f client.dockerfile .
 check_status
 
-# Ejecutar servidor
+# Run server
 echo -e "${BLUE}${ROCKET} Running server...${RESET}"
-# docker run --rm -it --name bitserver1 --cap-add NET_ADMIN --network bitservers bitserver
-# check_status
+docker run --rm -it --name bitserver1 --cap-add NET_ADMIN --network bitservers bitserver
+check_status
 
-# Ejecutar cliente
+# Run clients
 echo -e "${BLUE}${ROCKET} Running clients...${RESET}"
-# docker run --rm -it --name bitclient1 --cap-add NET_ADMIN --network bitclients bitclient
-# check_status
-# docker run --rm -it --name bitclient2 --cap-add NET_ADMIN --network bitclients bitclient
-# check_status
+docker run --rm -it --name bitclient1 --cap-add NET_ADMIN --network bitclients bitclient
+check_status
+docker run --rm -it --name bitclient2 --cap-add NET_ADMIN --network bitclients bitclient
+check_status
 
 echo -e "${GREEN}${CHECK_MARK} Setup complete! You can run everything now${RESET}"
