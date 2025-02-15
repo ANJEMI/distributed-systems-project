9a5ac1226d6dbb39fd40fc036f12638df83fc5d3
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2025-02-15 13:05:34 -0500

    add: eliminar residuos de uploads al clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -3,12 +3,16 @@
 # Define the paths
 CLIENT_1_DOWNLOADS="./src/client/downloads/client_1/*"
 CLIENT_2_DOWNLOADS="./src/client/downloads/client_2/*"
+CLIENT_1_UPLOADS="./src/client/uploads/client_1/*"
+CLIENT_2_UPLOADS="./src/client/uploads/client_2/*"
 TRACKER_DATA="./src/tracker/database/tracker_data.json"
 
 # Delete the contents of the client_1 and client_2 folders
 echo "Deleting contents of client_1 and client_2 in downloads..."
 rm -rf $CLIENT_1_DOWNLOADS
 rm -rf $CLIENT_2_DOWNLOADS
+rm -rf $CLIENT_1_UPLOADS
+rm -rf $CLIENT_2_UPLOADS
 
 # Delete the tracker_data.json file
 if [ -f "$TRACKER_DATA" ]; then
